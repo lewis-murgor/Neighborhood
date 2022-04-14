a24c9d9dbce36bcf0dc124b476c6daba1ef95c0a
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-14 23:34:23 +0300

    create models

--- location/models.py
+++ location/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length =100)
+    location = models.CharField(max_length =60)
+    occupants_count = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    health_contact = models.IntegerField()
+    police_contact = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    def update_neighborhood(self,updated_neighborhood):
+        self.neighborhood = updated_neighborhood
+        self.save()
+
+    def update_occupants(self,updated_occupants):
+        self.occupants = updated_occupants
+        self.save()
+
+    @classmethod
+    def find_neighborhood(cls,id):
+        neighborhood = cls.objects.filter(id = id)
+        return neighborhood
+
+class Business(models.Model):
+    name = models.CharField(max_length =100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    def update_business(self,updated_business):
+        self.business = updated_business
+        self.save()
+
+    @classmethod
+    def find_business(cls,id):
+        business = cls.objects.filter(id = id)
+        return business
