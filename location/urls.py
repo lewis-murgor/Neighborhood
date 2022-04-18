b47542aed8c372951bc7352b7cc77959d0738b44
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-18 19:24:26 +0300

    add urls

--- location/urls.py
+++ location/urls.py
@@ -2,10 +2,11 @@ from django.urls import re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
 urlpatterns=[
     re_path('^$',views.index,name='index'),
     re_path(r'^accounts/profile', views.profile, name='profile'),
+    re_path(r'^create/neighborhood$', views.create_neighborhood, name='create-neighborhood'),
+    re_path(r'^create/business/(\d+)', views.neighborhood, name='create-business'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
