3c881326e02b72d310595ca52638cb7b3596e4be
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-19 00:00:28 +0300

    create view functions

--- location/views.py
+++ location/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
-from .models import Neighborhood,Business
-from .forms import NewNeighborhoodForm,NewBusinessForm
+from .models import Neighborhood,Business, Post,Profile
+from .forms import NewNeighborhoodForm,NewBusinessForm,PostForm,UpdateProfileForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -14,8 +14,26 @@ def index(request):
 
 @login_required(login_url='/accounts/login/')
 def profile(request):
+    current_user = request.user
+    neighborhood = Neighborhood.objects.filter(user_id = current_user)
+    profile = Profile.objects.filter(user_id = current_user)
 
-    return render(request, 'profile.html')
+
+    return render(request, 'profile.html',{"hoods":neighborhood, "profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('profile')
+    else:
+        form = UpdateProfileForm()
+    return render(request, 'update_profile.html', {"form": form})
 
 @login_required(login_url='/accounts/login/')
 def create_neighborhood(request):
@@ -48,6 +66,55 @@ def neighborhood(request,neighborhood_id):
     else:
         form = NewBusinessForm()
     return render(request, 'create_business.html', {"form": form,"business":business,"neighborhood":neighborhood})
+
+@login_required(login_url='/accounts/login/')
+def create_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('post')
+
+    else:
+        form = PostForm()
+
+    return render(request, 'create_post.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def post(request):
+    post=Post.objects.all()
+
+    return render(request, 'post.html',{"posts": post})
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'business' in request.GET and request.GET["business"]:
+        name = request.GET.get("business")
+        searched_business = Business.search_by_name(name)
+        message = f"{name}"
+
+        return render(request, 'search.html',{"message":message,"business": searched_business})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def join(request):
+    current_user = request.user
+    if request.method == "POST":
+        neighborhood_id = request.POST.get('neighborhood_id')
+        neighborhood = Neighborhood.objects.get(id=neighborhood_id)
+
+        if current_user in neighborhood.joins.all():
+            neighborhood.joins.remove(current_user)
+        else:
+            neighborhood.joins.add(current_user)
+    return redirect('index')
+
 
 
 
