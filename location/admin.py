8c65f5b313dd79cf486e6ad4bc92db2db45c50a0
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-18 19:05:27 +0300

    add models

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Neighborhood,Business,Profile
 
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Business)
+admin.site.register(Profile)
